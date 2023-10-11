2fbbe648604fe41edeaae5b4d03c16f8db77c5f2
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2023-10-11 11:49:41 -0300

    Adição do metodo selecionarModelo

--- DAOZoologico/DAOAuxiliar.java
+++ DAOZoologico/DAOAuxiliar.java
@@ -20,7 +20,7 @@ public class DAOAuxiliar{
             } else {
                 a = true;
                 incrementaQuantidadeDoModelo(modelo);
-            }
+                }
             }
             
         } catch (SQLException e) {
@@ -39,6 +39,56 @@ public class DAOAuxiliar{
         e.printStackTrace();
     }
     
+    }
+
+    private void decrementaQuantidadeModelo(int modelo){
+        sql = "UPDATE controledejaulas SET quantidadeUtilizada = quantidadeUtilizada - 1 WHERE modelo = ?";
+    try {
+        PreparedStatement instrucao = conectar.prepareStatement(sql);
+        instrucao.setInt(1, modelo);
+        instrucao.executeUpdate();
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    }
+
+    public boolean removerModelo(int modelo){
+        sql = "SELECT COUNT(*) as quantidade FROM ControleDeJaulas WHERE modelo = ?";
+        boolean a = true;
+         try {
+            PreparedStatement instrucao = conectar.prepareStatement(sql);
+            instrucao.setInt(1, modelo);
+            ResultSet consulta = instrucao.executeQuery();
+            while(consulta.next()){
+            int quantidadeUtilizada = consulta.getInt("quantidade");
+            if(quantidadeUtilizada > 0){
+                a = true;
+                decrementaQuantidadeModelo(modelo);
+            } else {
+                a = false;
+                }
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return a;
+    }
+
+    public int selecionaModelo(int idAnimal){
+        int modelo = 0;
+        sql = "SELECT jaulas.modelo FROM animais JOIN jaulas ON animais.jaulaid = jaulas.idjaula WHERE idanimal = ?";
+        try {
+            PreparedStatement instrucao = conectar.prepareStatement(sql);
+            instrucao.setInt(1, idAnimal);
+            ResultSet consulta = instrucao.executeQuery();
+            while(consulta.next()){
+                modelo = consulta.getInt("modelo");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return modelo;
     }
 
 }
