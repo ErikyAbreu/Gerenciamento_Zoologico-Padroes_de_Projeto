7054abd0f29ed7d944d2c8eecb96ad77de15617c
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2023-10-16 09:29:20 -0300

    Modificações de metodos
    Modifiquei os metodos adiciona, imprimirAnimal e imprimirTodos para se encaixar com cada tipo de animal, pois alguns possuem parametros que que diferem um animal em especifico dos outros

--- DAOZoologico/DAOAnimais.java
+++ DAOZoologico/DAOAnimais.java
@@ -3,8 +3,7 @@ package DAOZoologico;
 import java.sql.*;
 
 import javax.swing.JOptionPane;
-
-import Animais.Animal;
+import Animais.*;
 
 public class DAOAnimais {
     private String sql; //String para armazenar comando do sql
@@ -21,7 +20,9 @@ public class DAOAnimais {
         //Se houver disponibilidade, adicionamos a jaula para o animal e recuperamos o id dessa jaula direto do banco de dados
         int idJaula = jaulas.AdicionaJaula(A.getJaula());
         //Inserindo comando para realização a inserção do animal na tabela animais do banco de dados
-        sql = "INSERT INTO animais (peso, membros, idade, nome,jaulaID) VALUES (?,?,?,?,?) RETURNING idAnimal";
+        if(A instanceof Anfibios){
+            
+        sql = "INSERT INTO animais (peso, membros, idade, nome,tipo,jaulaID) VALUES (?,?,?,?,?,?) RETURNING idAnimal";
         try {
             //Preparando o comando com os valores adequados para a tabela
             int idAnimal = 0;
@@ -30,7 +31,8 @@ public class DAOAnimais {
             instrucao.setInt(2, A.getMembros());
             instrucao.setInt(3, A.getIdade());
             instrucao.setString(4, A.getNome());
-            instrucao.setInt(5, idJaula);
+            instrucao.setString(5, "Anfibio");
+            instrucao.setInt(6, idJaula);
             //Executando comando
             ResultSet consulta = instrucao.executeQuery();
             if(consulta.next()){
@@ -45,11 +47,124 @@ public class DAOAnimais {
             e.printStackTrace();
         }
 
-        } else {
+        } else if(A instanceof Aves){
+        
+         sql = "INSERT INTO animais (peso, membros, idade, nome, tipo, cor_da_pena, jaulaID) VALUES (?,?,?,?,?,?,?) RETURNING idAnimal";
+         Aves Aves = (Aves) A;
+        try {
+            
+            //Preparando o comando com os valores adequados para a tabela
+            int idAnimal = 0;
+            PreparedStatement instrucao = conectar.prepareStatement(sql);
+            instrucao.setDouble(1, A.getPeso());
+            instrucao.setInt(2, A.getMembros());
+            instrucao.setInt(3, A.getIdade());
+            instrucao.setString(4, A.getNome());
+            instrucao.setString(5, "Ave");
+            instrucao.setString(6, Aves.getCordapena());
+            instrucao.setInt(7, idJaula);
+            //Executando comando
+            ResultSet consulta = instrucao.executeQuery();
+            if(consulta.next()){
+                idAnimal = consulta.getInt("idAnimal");
+                jaulas.atualizaIdDaJaula(idJaula,idAnimal);
+                JOptionPane.showMessageDialog(null,"Animal adicionado com sucesso a jaula de modelo "+A.getJaula().getModelo());
+                a = true;
+            }
+            
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+        } else if (A instanceof Mamifero){
+            Mamifero mamifero = (Mamifero) A;
+            sql = "INSERT INTO animais (peso, membros, idade, nome, tipo, cor_do_pelo, gestacao, jaulaID) VALUES (?,?,?,?,?,?,?,?) RETURNING idAnimal";
+        try {
+            //Preparando o comando com os valores adequados para a tabela
+            int idAnimal = 0;
+            PreparedStatement instrucao = conectar.prepareStatement(sql);
+            instrucao.setDouble(1, A.getPeso());
+            instrucao.setInt(2, A.getMembros());
+            instrucao.setInt(3, A.getIdade());
+            instrucao.setString(4, A.getNome());
+            instrucao.setString(5, "Mamifero");
+            instrucao.setString(6, mamifero.getCordepelo());
+            instrucao.setInt(7, mamifero.getTempodegestacao());
+            instrucao.setInt(8, idJaula);
+            //Executando comando
+            ResultSet consulta = instrucao.executeQuery();
+            if(consulta.next()){
+                idAnimal = consulta.getInt("idAnimal");
+                jaulas.atualizaIdDaJaula(idJaula,idAnimal);
+                JOptionPane.showMessageDialog(null,"Animal adicionado com sucesso a jaula de modelo "+A.getJaula().getModelo());
+                a = true;
+            }
+            
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+        } else if (A instanceof Peixes){
+             Peixes peixes = (Peixes) A;
+            sql = "INSERT INTO animais (peso, membros, idade, nome, tipo, cor_das_escamas, jaulaID) VALUES (?,?,?,?,?,?,?) RETURNING idAnimal";
+        try {
+            //Preparando o comando com os valores adequados para a tabela
+            int idAnimal = 0;
+            PreparedStatement instrucao = conectar.prepareStatement(sql);
+            instrucao.setDouble(1, A.getPeso());
+            instrucao.setInt(2, A.getMembros());
+            instrucao.setInt(3, A.getIdade());
+            instrucao.setString(4, A.getNome());
+            instrucao.setString(5, "Peixe");
+            instrucao.setString(6, peixes.getCordeescama());
+            instrucao.setInt(7, idJaula);
+            //Executando comando
+            ResultSet consulta = instrucao.executeQuery();
+            if(consulta.next()){
+                idAnimal = consulta.getInt("idAnimal");
+                jaulas.atualizaIdDaJaula(idJaula,idAnimal);
+                JOptionPane.showMessageDialog(null,"Animal adicionado com sucesso a jaula de modelo "+A.getJaula().getModelo());
+                a = true;
+            }
+            
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+        } else if (A instanceof Repteis){
+            Repteis repteis = (Repteis) A;
+            sql = "INSERT INTO animais (peso, membros, idade, nome, tipo, cor_das_escamas, jaulaID) VALUES (?,?,?,?,?,?,?) RETURNING idAnimal";
+        try {
+            //Preparando o comando com os valores adequados para a tabela
+            int idAnimal = 0;
+            PreparedStatement instrucao = conectar.prepareStatement(sql);
+            instrucao.setDouble(1, A.getPeso());
+            instrucao.setInt(2, A.getMembros());
+            instrucao.setInt(3, A.getIdade());
+            instrucao.setString(4, A.getNome());
+            instrucao.setString(5, "Reptil");
+            instrucao.setString(6, repteis.getCordeescama());
+            instrucao.setInt(7, idJaula);
+            //Executando comando
+            ResultSet consulta = instrucao.executeQuery();
+            if(consulta.next()){
+                idAnimal = consulta.getInt("idAnimal");
+                jaulas.atualizaIdDaJaula(idJaula,idAnimal);
+                JOptionPane.showMessageDialog(null,"Animal adicionado com sucesso a jaula de modelo "+A.getJaula().getModelo());
+                a = true;
+            }
+            
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+        }
+
+    } else {
             JOptionPane.showMessageDialog(null,"Não há espaço para adicionar o animal no modelo de jaula selecionado");
             a = false;
         }
-        
+      
         return a;
     }
 
@@ -88,11 +203,26 @@ public class DAOAnimais {
                   int membros = consulta.getInt("membros");
                   int idade = consulta.getInt("idade");
                   String nome = consulta.getString("nome");
+                  String tipoDoAnimal = consulta.getString("tipo");
+                  String corDaPena = consulta.getString("cor_da_pena");
+                  String corDasEscamas = consulta.getString("cor_das_escamas");
+                  String corDoPelo = consulta.getString("cor_do_pelo");
+                  int gestacao = consulta.getInt("gestacao");
                   int idAnimalJaula = consulta.getInt("jaulaid");
                   int modelo = consulta.getInt("modelo");
                   String descricaoJaula = consulta.getString("descricao");
-
-                  JOptionPane.showMessageDialog(null, "DADOS DO ANIMAL\nID do animal: "+animalId+" \nPeso do animal: "+peso+"\nQuantidade de membros: "+membros+"\nIdade do animal: "+idade+"\nNome do animal: "+nome+"\n\nDADOS DA JAULA\nID da jaula: "+idAnimalJaula+"\nModelo da jaula: "+modelo+"\nDescricao da jaula: \n"+descricaoJaula);
+                  if(tipoDoAnimal.equals("Anfibio")){
+                    JOptionPane.showMessageDialog(null, "DADOS DO ANIMAL\nID do animal: "+animalId+"\nTipo do animal: "+tipoDoAnimal+"\nPeso do animal: "+peso+"\nQuantidade de membros: "+membros+"\nIdade do animal: "+idade+"\nNome do animal: "+nome+"\n\nDADOS DA JAULA\nID da jaula: "+idAnimalJaula+"\nModelo da jaula: "+modelo+"\nDescricao da jaula: \n"+descricaoJaula);
+                  } else if(tipoDoAnimal.equals("Ave")){
+                    JOptionPane.showMessageDialog(null, "DADOS DO ANIMAL\nID do animal: "+animalId+"\nTipo do animal: "+tipoDoAnimal+"\nPeso do animal: "+peso+"\nQuantidade de membros: "+membros+"\nIdade do animal: "+idade+"\nCor das penas: "+corDaPena+"\nNome do animal: "+nome+"\n\nDADOS DA JAULA\nID da jaula: "+idAnimalJaula+"\nModelo da jaula: "+modelo+"\nDescricao da jaula: \n"+descricaoJaula);
+                  } else if(tipoDoAnimal.equals("Mamifero")){
+                    JOptionPane.showMessageDialog(null, "DADOS DO ANIMAL\nID do animal: "+animalId+"\nTipo do animal: "+tipoDoAnimal+"\nPeso do animal: "+peso+"\nQuantidade de membros: "+membros+"\nIdade do animal: "+idade+"\nCor dos pelos: "+corDoPelo+"\nTempo de gestação: "+gestacao+"\nNome do animal: "+nome+"\n\nDADOS DA JAULA\nID da jaula: "+idAnimalJaula+"\nModelo da jaula: "+modelo+"\nDescricao da jaula: \n"+descricaoJaula);
+                  } else if(tipoDoAnimal.equals("Peixe")){
+                    JOptionPane.showMessageDialog(null, "DADOS DO ANIMAL\nID do animal: "+animalId+"\nTipo do animal: "+tipoDoAnimal+"\nPeso do animal: "+peso+"\nQuantidade de membros: "+membros+"\nIdade do animal: "+idade+"\nCor das escamas: "+corDasEscamas+"\nNome do animal: "+nome+"\n\nDADOS DA JAULA\nID da jaula: "+idAnimalJaula+"\nModelo da jaula: "+modelo+"\nDescricao da jaula: \n"+descricaoJaula);
+                  } else if(tipoDoAnimal.equals("Reptil")){
+                    JOptionPane.showMessageDialog(null, "DADOS DO ANIMAL\nID do animal: "+animalId+"\nTipo do animal: "+tipoDoAnimal+"\nPeso do animal: "+peso+"\nQuantidade de membros: "+membros+"\nIdade do animal: "+idade+"\nCor das escamas: "+corDasEscamas+"\nNome do animal: "+nome+"\n\nDADOS DA JAULA\nID da jaula: "+idAnimalJaula+"\nModelo da jaula: "+modelo+"\nDescricao da jaula: \n"+descricaoJaula);
+                  }
+                  
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -110,11 +240,25 @@ public class DAOAnimais {
                   int membros = consulta.getInt("membros");
                   int idade = consulta.getInt("idade");
                   String nome = consulta.getString("nome");
+                  String tipoDoAnimal = consulta.getString("tipo");
+                  String corDaPena = consulta.getString("cor_da_pena");
+                  String corDasEscamas = consulta.getString("cor_das_escamas");
+                  String corDoPelo = consulta.getString("cor_do_pelo");
+                  int gestacao = consulta.getInt("gestacao");
                   int idAnimalJaula = consulta.getInt("jaulaid");
                   int modelo = consulta.getInt("modelo");
                   String descricaoJaula = consulta.getString("descricao");
-
-                  JOptionPane.showMessageDialog(null, "DADOS DO ANIMAL\nID do animal: "+animalId+" \nPeso do animal: "+peso+"\nQuantidade de membros: "+membros+"\nIdade do animal: "+idade+"\nNome do animal: "+nome+"\n\nDADOS DA JAULA\nID da jaula: "+idAnimalJaula+"\nModelo da jaula: "+modelo+"\nDescricao da jaula: \n"+descricaoJaula);
+                  if(tipoDoAnimal.equals("Anfibio")){
+                    JOptionPane.showMessageDialog(null, "DADOS DO ANIMAL\nID do animal: "+animalId+"\nTipo do animal: "+tipoDoAnimal+"\nPeso do animal: "+peso+"\nQuantidade de membros: "+membros+"\nIdade do animal: "+idade+"\nNome do animal: "+nome+"\n\nDADOS DA JAULA\nID da jaula: "+idAnimalJaula+"\nModelo da jaula: "+modelo+"\nDescricao da jaula: \n"+descricaoJaula);
+                  } else if(tipoDoAnimal.equals("Ave")){
+                    JOptionPane.showMessageDialog(null, "DADOS DO ANIMAL\nID do animal: "+animalId+"\nTipo do animal: "+tipoDoAnimal+"\nPeso do animal: "+peso+"\nQuantidade de membros: "+membros+"\nIdade do animal: "+idade+"\nCor das penas: "+corDaPena+"\nNome do animal: "+nome+"\n\nDADOS DA JAULA\nID da jaula: "+idAnimalJaula+"\nModelo da jaula: "+modelo+"\nDescricao da jaula: \n"+descricaoJaula);
+                  } else if(tipoDoAnimal.equals("Mamifero")){
+                    JOptionPane.showMessageDialog(null, "DADOS DO ANIMAL\nID do animal: "+animalId+"\nTipo do animal: "+tipoDoAnimal+"\nPeso do animal: "+peso+"\nQuantidade de membros: "+membros+"\nIdade do animal: "+idade+"\nCor dos pelos: "+corDoPelo+"\nTempo de gestação: "+gestacao+"\nNome do animal: "+nome+"\n\nDADOS DA JAULA\nID da jaula: "+idAnimalJaula+"\nModelo da jaula: "+modelo+"\nDescricao da jaula: \n"+descricaoJaula);
+                  } else if(tipoDoAnimal.equals("Peixe")){
+                    JOptionPane.showMessageDialog(null, "DADOS DO ANIMAL\nID do animal: "+animalId+"\nTipo do animal: "+tipoDoAnimal+"\nPeso do animal: "+peso+"\nQuantidade de membros: "+membros+"\nIdade do animal: "+idade+"\nCor das escamas: "+corDasEscamas+"\nNome do animal: "+nome+"\n\nDADOS DA JAULA\nID da jaula: "+idAnimalJaula+"\nModelo da jaula: "+modelo+"\nDescricao da jaula: \n"+descricaoJaula);
+                  } else if(tipoDoAnimal.equals("Reptil")){
+                    JOptionPane.showMessageDialog(null, "DADOS DO ANIMAL\nID do animal: "+animalId+"\nTipo do animal: "+tipoDoAnimal+"\nPeso do animal: "+peso+"\nQuantidade de membros: "+membros+"\nIdade do animal: "+idade+"\nCor das escamas: "+corDasEscamas+"\nNome do animal: "+nome+"\n\nDADOS DA JAULA\nID da jaula: "+idAnimalJaula+"\nModelo da jaula: "+modelo+"\nDescricao da jaula: \n"+descricaoJaula);
+                  }
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
