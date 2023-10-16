2abbb9b1e2d13e2af7d163f95fe7994998a6c9e5
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2023-10-16 13:54:59 -0300

    Atualização da fabrica
    atualizei o metodo de modo que não é mais necessario passar o numero da sala

--- Funcionarios/FactoryFuncionario.java
+++ Funcionarios/FactoryFuncionario.java
@@ -9,13 +9,13 @@ public class FactoryFuncionario {
 			return (new Gerente(JOptionPane.showInputDialog(null,"NOME DO GERENTE: ","GERENTE", JOptionPane.INFORMATION_MESSAGE), 
 						Integer.parseInt(JOptionPane.showInputDialog(null,"MATRÍCULA DO GERENTE: ","GERENTE", JOptionPane.INFORMATION_MESSAGE)),
 						Double.parseDouble(JOptionPane.showInputDialog(null,"SALÁRIO BASE DO GERENTE: ","GERENTE", JOptionPane.INFORMATION_MESSAGE)),  
-						new Sala(Integer.parseInt(JOptionPane.showInputDialog(null,"INFORME A SALA DO GERENTE: ","GERENTE", JOptionPane.INFORMATION_MESSAGE)), 1))); 
+						new Sala(1))); 
 						
 			case 2:
 			return (new Veterinario(JOptionPane.showInputDialog(null,"NOME DO VETERINÁRIO: ","VETERINÁRIO", JOptionPane.INFORMATION_MESSAGE), 
 						Integer.parseInt(JOptionPane.showInputDialog(null,"MATRÍCULA DO VETERINÁRIO: ","VETERINÁRIO", JOptionPane.INFORMATION_MESSAGE)),
 						Double.parseDouble(JOptionPane.showInputDialog(null,"SALÁRIO BASE DO VETERINÁRIO: ","VETERINÁRIO", JOptionPane.INFORMATION_MESSAGE)),  
-						new Sala(Integer.parseInt(JOptionPane.showInputDialog(null,"INFORME A SALA DO VETERINÁRIO: ","VETERINÁRIO", JOptionPane.INFORMATION_MESSAGE)), 2),
+						new Sala(2),
 						250,
 						Integer.parseInt(JOptionPane.showInputDialog(null,"QUANTIDADE DE ANIMAIS ATENDIDOS: ","VETERINÁRIO", JOptionPane.INFORMATION_MESSAGE)))); 
 			
@@ -23,7 +23,7 @@ public class FactoryFuncionario {
 			return (new Zelador(JOptionPane.showInputDialog(null,"NOME DO ZELADOR: ","ZELADOR", JOptionPane.INFORMATION_MESSAGE), 
 						Integer.parseInt(JOptionPane.showInputDialog(null,"MATRÍCULA DO ZELADOR: ","ZELADOR", JOptionPane.INFORMATION_MESSAGE)),
 						Double.parseDouble(JOptionPane.showInputDialog(null,"SALÁRIO BASE DO ZELADOR: ","ZELADOR", JOptionPane.INFORMATION_MESSAGE)),  
-						new Sala(Integer.parseInt(JOptionPane.showInputDialog(null,"INFORME A SALA DO ZELADOR: ","ZELADOR", JOptionPane.INFORMATION_MESSAGE)), 3))); 
+						new Sala(3))); 
 			
 			default:
 			JOptionPane.showMessageDialog(null, "OPÇÃO INVÁLIDA! TENTE NOVAMENTE.");
