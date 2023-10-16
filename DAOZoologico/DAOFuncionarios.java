71e56346000dd11fa81c819edc3bb91e48207a77
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2023-10-16 13:45:15 -0300

    Metodo para adicionar/cadastrar funcionario
    Implementação de metodo para cadastrar um funcionario no banco de dados

--- DAOZoologico/DAOFuncionarios.java
+++ DAOZoologico/DAOFuncionarios.java
@@ -5,25 +5,56 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import Funcionarios.Funcionario;
 import Funcionarios.Veterinario;
 
 public class DAOFuncionarios {
-    String sql;
+    private String sql;
     private Connection conectar = ConexaoPostgreSQL.getInstancia().getConexao(); //Classe para estabelecer uma conexão ao banco de dados
+    private boolean a;
+    private DAOAuxiliar auxiliar = new DAOAuxiliar();
+
+    public boolean addFuncionario(Funcionario f){
+        a = false;
+        int idSala = auxiliar.adicionaSalaParaFuncionario(f.getSala());
+        int idFunc = 0;
+        System.out.println(idSala);
+        sql = "INSERT INTO funcionarios (nome,cargo,matricula,sala,salario_base) VALUES (?,?,?,?,?) RETURNING id";
+        try {
+            PreparedStatement instrucao = conectar.prepareStatement(sql);
+            instrucao.setString(1, f.getNome());
+            instrucao.setString(2, f.getClass().getSimpleName());
+            instrucao.setInt(3, f.getMatricula());
+            instrucao.setInt(4, idSala);
+            instrucao.setDouble(5, f.getSalario_base());
+            ResultSet consulta = instrucao.executeQuery();
+            if(consulta.next()){
+                idFunc = consulta.getInt("id");
+                auxiliar.atualizaIdDaSala(idSala, idFunc);
+                a = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return a;
+    }
+
 
     public Veterinario getVeterinario(int matricula){
-    Veterinario vet = new Veterinario();
-    sql = "SELECT * FROM funcionario WHERE matricula = ? AND cargo = 'veterinario'";
+    Veterinario vet = null;
+    sql = "SELECT * FROM funcionarios WHERE matricula = ? AND cargo = 'Veterinario'";
     try {
         PreparedStatement instrucao = conectar.prepareStatement(sql);
         instrucao.setInt(1, matricula);
         ResultSet consulta = instrucao.executeQuery();
-        while(consulta.next()){
-            int idDoVet = consulta.getInt("id");
+        if(consulta.next()){
+           // int idDoVet = consulta.getInt("id");
             String nome = consulta.getString("nome");
             int matriculaVet = consulta.getInt("matricula");
             double salarioBase = consulta.getDouble("salario_base");
             vet = new Veterinario(nome, matricula, salarioBase, null, salarioBase, matriculaVet);
+        } else {
+
         }
 
         
